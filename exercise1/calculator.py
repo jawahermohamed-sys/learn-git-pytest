632952a28e0b834e8697f50c0eaa926add339b89
jawahermohamed-sys <jawahermohamed.ma@gmail.com>
2026-03-10 10:39:03 +0000

    Update calculator.py

--- exercise1/calculator.py
+++ exercise1/calculator.py
@@ -16,6 +16,7 @@ def add(a: Number, b: Number) -> Number:
     Returns:
         The sum of a and b
     """
+    return a+b
     # TODO: Implement this function
     pass
 
@@ -31,6 +32,7 @@ def subtract(a: Number, b: Number) -> Number:
     Returns:
         The result of a - b
     """
+    return a-b
     # TODO: Implement this function
     pass
 
@@ -46,6 +48,7 @@ def multiply(a: Number, b: Number) -> Number:
     Returns:
         The product of a and b
     """
+    return a*b
     # TODO: Implement this function
     pass
 
@@ -64,5 +67,7 @@ def divide(a: Number, b: Number) -> Number:
     Raises:
         ValueError: If b is 0
     """
-    # TODO: Implement this function
+    assert b != 0
+    return a/b
+    #TODO: Implement this function
     pass
